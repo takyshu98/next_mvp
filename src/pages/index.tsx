f86530424b13ea081f72ff0244aa9075b07e48a1
takyshu98 <shuhei.takiguchi.0908@gmail.com>
2023-08-15 16:17:18 +0900

    add button - component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,14 @@
-export default function Page() {
-  return <h1>Hello, Next.js!</h1>;
-}
+import { Button } from '@/components/button';
+
+const LandingPage = () => {
+  return (
+    <>
+      <h1>Hello, Next.js!</h1>
+      <Button variant="solid" type="button">
+        Click Me
+      </Button>
+    </>
+  );
+};
+
+export default LandingPage;
